df6e5d1db0ecd1778a5c5475d8ea35e6fb73f307
anabellesch <127800156+anabellesch@users.noreply.github.com>
2023-04-24 20:38:53 -0300

    Update DateFormatterTests.cs

--- src/tests/Library.Tests/DateFormatterTests.cs
+++ src/tests/Library.Tests/DateFormatterTests.cs
@@ -8,8 +8,25 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void FechaCorrecta()
     {
-        Assert.Pass();
+        const string input = "23/04/2023";
+        const string expected = "23-04-2023";
+        string actual = DateFormatter.ChangeFormat(input);
+    }
+    public void FechaIncorrecta()
+    {
+        const string input = "24-04-2023";
+        const string expected = "24/04/2023";
+        string actual = DateFormatter.ChangeFormat(input);
+
+
+    }
+    public void FechaVacia()
+    {
+        const string input = "";
+        const string expected = "Error";
+        string actual = DateFormatter.ChangeFormat(input);
+
     }
 }
